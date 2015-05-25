d9b766a173fe683402a8456a74ac6a939a221e73
DavidLP <pohl@physik.uni-bonn.de>
2015-05-25 21:03:29 +0200

    MAINT: formatting

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -7,26 +7,24 @@
 #include "G4THitsMap.hh"
 #include "globals.hh"
 
-class EventAction : public G4UserEventAction
-{
-public:
-  EventAction();
-  virtual ~EventAction();
+class EventAction: public G4UserEventAction {
+	public:
+		EventAction();
+		virtual ~EventAction();
 
-  virtual void  BeginOfEventAction(const G4Event* event);
-  virtual void  EndOfEventAction(const G4Event* event);
-    
-private:
-  // methods
-  G4THitsMap<G4double>* GetHitsCollection(G4int hcID, const G4Event* event) const;
-  DetHitsMap* GetPixelHitsMap(G4int hcID, const G4Event* event) const;
-  G4double GetSum(G4THitsMap<G4double>* hitsMap) const;
-  void PrintEventStatistics(G4double edep, G4double trackLength) const;
-  
-  // data members                   
-  G4int  fSensorEdepHCID, fSensorTrackLengthHCID, fSensorTrackAngleInHCID, fSensorTrackAngleOutHCID, fTriggerHCID, fShieldInHCID, fShieldOutHCID, fPixelDetectorHCID;
+		virtual void BeginOfEventAction(const G4Event* event);
+		virtual void EndOfEventAction(const G4Event* event);
+
+	private:
+		// methods
+		G4THitsMap<G4double>* GetHitsCollection(G4int hcID, const G4Event* event) const;
+		DetHitsMap* GetPixelHitsMap(G4int hcID, const G4Event* event) const;
+		G4double GetSum(G4THitsMap<G4double>* hitsMap) const;
+		void PrintEventStatistics(G4double edep, G4double trackLength) const;
+
+		// data members
+		G4int fSensorEdepHCID, fSensorTrackLengthHCID, fSensorTrackAngleInHCID, fSensorTrackAngleOutHCID, fTriggerHCID, fShieldInHCID, fShieldOutHCID, fPixelDetectorHCID;
 };
 
 #endif
 
-    
