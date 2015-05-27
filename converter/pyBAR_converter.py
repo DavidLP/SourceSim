6b9a07955a75242532a5de94850c3cf2808cb7dc
DavidLP <pohl@physik.uni-bonn.de>
2015-05-27 20:52:48 +0200

    ENH: add first attempt for charge digitization (TOT TDC). Needs bugfixing and speed up

--- converter/pyBAR_converter.py
+++ converter/pyBAR_converter.py
@@ -5,12 +5,29 @@ import numpy as np
 import ctypes
 import progressbar
 import os
+import math
 
 import ROOT as r
+from pybar.fei4 import register_utils
 from pybar.analysis.RawDataConverter import data_struct
 
 
-def create_hit_table(input_file_name, n_sub_files=8):  # loops over all root files and merges the data into a hdf5 file aligned at the event number
+def get_charge_calibration(tdc_calibation_file, plsr_dac_calibation_file):
+    with tb.openFile(tdc_calibation_file, mode="r") as in_file_calibration_h5:
+        tdc_calibration, tdc_error = in_file_calibration_h5.root.HitOrCalibration[:, :, :, 1], in_file_calibration_h5.root.HitOrCalibration[:, :, :, 3]
+        tot_calibration, tot_error = in_file_calibration_h5.root.HitOrCalibration[:, :, :, 0], in_file_calibration_h5.root.HitOrCalibration[:, :, :, 2]
+        tdc_calibration_values = in_file_calibration_h5.root.HitOrCalibration.attrs.scan_parameter_values[:]
+        global_config = register_utils.parse_global_config(plsr_dac_calibation_file)
+        c_high, vcal_c0, vcal_c1 = global_config['C_Inj_High'], global_config['Vcal_Coeff_0'], global_config['Vcal_Coeff_1']
+        charge_calibration_values = (vcal_c0 + vcal_c1 * tdc_calibration_values) * c_high / 0.16022
+
+    return charge_calibration_values, tdc_calibration, tdc_error, tot_calibration, tot_error
+
+
+def create_hit_table(input_file_name, tdc_calibation_file, plsr_dac_calibation_file, n_sub_files=8):  # loops over all root files and merges the data into a hdf5 file aligned at the event number
+    print 'Converting data from CERN ROOT TTree to hdf5 table'
+    charge_calibration_values, tdc_calibration, tdc_error, tot_calibration, tot_error = get_charge_calibration(tdc_calibation_file, plsr_dac_calibation_file)
+
     # add all files that have the input_file_name praefix and load their data
     input_file_names = [input_file_name + '_t%d.root' % index for index in range(n_sub_files) if os.path.isfile(input_file_name + '_t%d.root' % index)]
     n_files = len(input_file_names)
@@ -46,35 +63,54 @@ def create_hit_table(input_file_name, n_sub_files=8):  # loops over all root fil
 
         actual_data = data[actual_file_index]
         actual_event_number = actual_data['event'][0]
+        last_valid_event_number = 0
+        last_tdc = 0
         expected_event_number = actual_event_number
         indices[actual_file_index] = 1
 
         progress_bar = progressbar.ProgressBar(widgets=['', progressbar.Percentage(), ' ', progressbar.Bar(marker='*', left='|', right='|'), ' ', progressbar.AdaptiveETA()], maxval=n_total_hits, term_width=80)
         progress_bar.start()
-        
+
         def add_actual_data(actual_data, table_index):
             if actual_data['column'] >= 0 and actual_data['column'] < 80 and actual_data['row'] >= 0 and actual_data['row'] < 336:
+                tdc_interpolation = interp1d(x=charge_calibration_values, y=tdc_calibration[actual_data['column'], actual_data['row']], kind='slinear', bounds_error=False, fill_value=0)
+                tdc = tdc_interpolation(actual_data['charge'])
+                tot_interpolation = interp1d(x=charge_calibration_values, y=tot_calibration[actual_data['column'], actual_data['row']], kind='slinear', bounds_error=False, fill_value=0)
+                tot = tot_interpolation(actual_data['charge'])
+
+                if math.isnan(tdc):  # do not add hits where tdc is nan, these pixel have a very high threshold or do not work
+                    return table_index
+
+                if tdc == 0 and actual_data['charge'] > 10000:  # no calibration for TDC due to high charge, thus mark as TDC overflow event
+                    hits[table_index]['event_status'] |= 0b0000010000000000
+                    tdc = 4095
+
+                if tot == 0 and actual_data['charge'] > 10000:  # no calibration for TOT due to high charge, thus set max tot
+                    tot = 13
+
+                hits[table_index]['event_status'] |= 0b0000000100000000
                 hits[table_index]['event_number'] = actual_data['event'][0].astype(np.int64)
                 hits[table_index]['column'] = (actual_data['column'] + 1).astype(np.uint8)
                 hits[table_index]['row'] = (actual_data['row'] + 1).astype(np.uint16)
-                hits[table_index]['tot'] = 1
-                hits[table_index]['LVL1ID'] = hits[table_index]['event_number'] % 255
-                hits[table_index]['BCID'] = 6
+                hits[table_index]['TDC'] = int(actual_data['charge'] / 300.)
+                hits[table_index]['tot'] = int(tot)
+
+                table_index += 1
+            return table_index
 
         while True:
             actual_event_number = actual_data['event'][0]
             if (actual_event_number == expected_event_number or actual_event_number == expected_event_number - 1):  # check if event number increases
                 actual_index, actual_digits, actual_data = indices[actual_file_index], pixel_digits[actual_file_index], data[actual_file_index]
-                add_actual_data(actual_data, table_index)
+                table_index = add_actual_data(actual_data, table_index)
             else:  # event number does not increase, thus the events are in another file --> switch file or the event number is missing
                 file_event_numbers = [data[file_index]['event'][0] for file_index in range(n_files)]  # all files actual event number
                 actual_file_index = np.where(file_event_numbers == min(file_event_numbers))[0][0]
                 actual_index, actual_digits, actual_data = indices[actual_file_index], pixel_digits[actual_file_index], data[actual_file_index]
                 actual_event_number = actual_data['event'][0]
-                add_actual_data(actual_data, table_index)
+                table_index = add_actual_data(actual_data, table_index)
 
             progress_bar.update(table_index)
-            table_index += 1
             expected_event_number = actual_event_number + 1
             actual_digits.GetEntry(actual_index)
 
@@ -83,6 +119,10 @@ def create_hit_table(input_file_name, n_sub_files=8):  # loops over all root fil
             else:
                 break
 
+        # Set missing data and store to file
+        hits[:table_index]['LVL1ID'] = hits[:table_index]['event_number'] % 255
+        hits[:table_index]['BCID'] = hits[:table_index]['LVL1ID']
+        hits[:table_index]['relative_BCID'] = 6
         hit_table.append(hits[:table_index])
 
         progress_bar.finish()
@@ -92,4 +132,32 @@ def create_hit_table(input_file_name, n_sub_files=8):  # loops over all root fil
 
 
 if __name__ == "__main__":
-    create_hit_table(r'/home/davidlp/geant4/SourceSim-build/SourceSimulation')
+    import matplotlib.pyplot as plt
+    from scipy.interpolate import interp1d
+
+    input_file_name = r'/home/davidlp/geant4/SourceSim-build/SourceSimulation'
+    plsr_dac_calibation_file = r'/home/davidlp/git/SourceSim/converter/calibration_data/plsr_dac_scan.cfg'
+    tdc_calibation_file = r'/home/davidlp/git/SourceSim/converter/calibration_data/hit_or_calibration_calibration.h5'
+
+    create_hit_table(input_file_name, tdc_calibation_file, plsr_dac_calibation_file)
+
+#     with tb.openFile(tdc_calibation_file, mode="r") as in_file_calibration_h5:
+#         tdc_calibration, tdc_error = in_file_calibration_h5.root.HitOrCalibration[:, :, :, 1], in_file_calibration_h5.root.HitOrCalibration[:, :, :, 3]
+#         tot_calibration, tot_error = in_file_calibration_h5.root.HitOrCalibration[:, :, :, 0], in_file_calibration_h5.root.HitOrCalibration[:, :, :, 2]
+#         tdc_calibration_values = in_file_calibration_h5.root.HitOrCalibration.attrs.scan_parameter_values[:]
+#         global_config = register_utils.parse_global_config(plsr_dac_calibation_file)
+#         c_high, vcal_c0, vcal_c1 = global_config['C_Inj_High'], global_config['Vcal_Coeff_0'], global_config['Vcal_Coeff_1']
+#         charge_calibration_values = (vcal_c0 + vcal_c1 * tdc_calibration_values) * c_high / 0.16022
+#
+#         column, row = 20, 30
+#         print charge_calibration_values, tdc_calibration[column, row]
+#         tdc_interpolation = interp1d(x=charge_calibration_values, y=tdc_calibration[column, row], kind='slinear', bounds_error=False, fill_value=0)
+#         tdc_error_interpolation = interp1d(x=charge_calibration_values, y=tdc_error[column, row], kind='slinear', bounds_error=False, fill_value=0)
+#         tot_interpolation = interp1d(x=charge_calibration_values, y=tot_calibration[column, row], kind='slinear', bounds_error=False, fill_value=0)
+#         tot_error_interpolation = interp1d(x=charge_calibration_values, y=tot_error[column, row], kind='slinear', bounds_error=False, fill_value=0)
+
+#         plt.plot(np.arange(2000, 10000, 1), tdc_interpolation(np.arange(2000, 10000, 1)), '-')
+#         plt.plot(np.arange(2000, 10000, 1), tdc_error_interpolation(np.arange(2000, 10000, 1)), '-')
+#         plt.plot(np.arange(2000, 10000, 1), tot_interpolation(np.arange(2000, 10000, 1)), '-')
+#         plt.plot(np.arange(2000, 10000, 1), tot_error_interpolation(np.arange(2000, 10000, 1)), '-')
+#         plt.show()
