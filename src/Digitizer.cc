8cdd7aadc01d8a4dd59ea7dae8da64ee8667409d
DavidLP <pohl@physik.uni-bonn.de>
2015-07-31 14:18:55 +0200

    ENH: initial charge cloud at t = 0 with non zero sigma is supported ENH: charge cloud sigma due to diffusion can be scaled up to take repulsion into account. Very simple first order approximation ENH: PrintSettings shows more settings API: set function for initial charge cloud sigma and charge cloud sigma correction factor

--- src/Digitizer.cc
+++ src/Digitizer.cc
@@ -25,7 +25,9 @@ Digitizer::Digitizer(G4String name) :
 		G4VDigitizerModule(name), fCalcChargeCloud(true), fReadOutDirection(true), fEnergyPerCharge(3.74), fThreshold(2000),  // std. IBL detector
 		fNoise(130),  // std. IBL detector
 		fTemperatur(330),  // 56.8 C (uncooled 50-60 is common)
-		fBias(60.),
+		fBias(60.),  // bias of the sensor in volt
+		fSigma0(2.), // initial charge cloud gaussian profile sigma
+		fSigmaCC(1.3), // correction factor for charge cloud sigma to take into account diffusion + repulsion
 		fPixelDigitsCollection(0)
 {
 	G4String colName = "PixelDigitsCollection";
@@ -93,12 +95,13 @@ void Digitizer::AddHitToDigits(std::map<G4int, DetHit*>::const_iterator iHit, Pi
 	G4double charge = iHit->second->GetEdep() / fEnergyPerCharge / eV;  // charge in electrons
 	G4ThreeVector position = iHit->second->GetPosition();  // are negative in z or not?
 
-	double z = 0;  // z position in sensor [0..thickness]
-	double fraction = 0;  // fraction of the total charge for the actual pixel
-	double minFraction = 1e-5;
-	double totalFraction = 0;
-
 	if (fCalcChargeCloud) {  // distribute the deposited charge into neighboring pixels
+		// temporary variables
+		double z = 0;  // z position in sensor [0..thickness]
+		double fraction = 0;  // fraction of the total charge for the actual pixel
+		double minFraction = 1e-5;  //  minimum charge fraction to add charge to the actual pixel
+		double totalFraction = 0;  // total fraction of charge distributed (=1)
+
 		if (fReadOutDirection)  // calculate drift/read out electronics direction
 			z = fSensorThickness / 2. - position[2];  // RO after sensor in beam
 		else
@@ -109,6 +112,9 @@ void Digitizer::AddHitToDigits(std::map<G4int, DetHit*>::const_iterator iHit, Pi
 			z = 0.;
 		if (z > fSensorThickness)
 			z = fSensorThickness;
+
+		if (fSigma0 > 0)  // increase z to simulate a charge cloud with non zero spread at start
+			z = CalcZfromSigma(z);
 
 		if (z == 0) {  // special case. charge does not travel at all
 			AddChargeToDigits(column, row, charge, digits);
@@ -161,17 +167,16 @@ void Digitizer::AddHitToDigits(std::map<G4int, DetHit*>::const_iterator iHit, Pi
 			}
 		}
 	}
-	else
+	else{
 		AddChargeToDigits(column, row, charge, digits);
+	}
 }
 
 void Digitizer::AddChargeToDigits(const int& column, const int& row, const double& charge, PixelDigitsCollection* digits)
 {
 // Runtime ugly function, a two key hash map would be nice...
-//	if (G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID() == 11637)
-//		std::cout << "    AddChargeToDigits col/row/charge=" <<column<<"/"<<row<<"/"<<charge<<"\n";
 	bool digitExists = false;
-	for (G4int iDigi = 0; iDigi < digits->entries(); ++iDigi) {  // go through all already created digis
+	for (G4int iDigi = 0; iDigi < digits->entries(); ++iDigi) {  // go through all already created digis, if it exists add charge
 		if (column == (*digits)[iDigi]->GetColumn() && row == (*digits)[iDigi]->GetRow()) {
 			(*digits)[iDigi]->Add(charge);
 			digitExists = true;
@@ -179,7 +184,7 @@ void Digitizer::AddChargeToDigits(const int& column, const int& row, const doubl
 		}
 	}
 
-	if (!digitExists) {  // pixel digi does not exist, thus create new
+	if (!digitExists) {  // pixel digi does not exist, thus create new digit with given charge
 		PixelDigi* digi = new PixelDigi(column, row, charge);
 		digits->insert(digi);
 	}
@@ -194,25 +199,11 @@ double Digitizer::CalcChargeFraction(const double& x, const double& y, const dou
 	 voltage : the applied voltage
 	 temperature : the temperature in Kelvin
 	 */
+	double sigma = CalcSigmaDiffusion(z, voltage, temperature) * fSigmaCC;  // apply correction factor to take repulsion into account
 
-	double sigma = CalcSigmaDiffusion(z, voltage, temperature);
-
-//	if (G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID() == 11637){
-//		std::cout << "    CalcChargeFraction " <<"\n";
-//		std::cout << "      x/y/z " << G4BestUnit(x, "Length")<<"/"<< G4BestUnit(y, "Length")<<"/"<<G4BestUnit(z, "Length")<<"\n";
-//		std::cout << "        "<<G4BestUnit(x, "Length")<<" "<<G4BestUnit(y, "Length")<<" "<<G4BestUnit(z, "Length")<<" "<<G4BestUnit(x_pitch, "Length")<<" "<<G4BestUnit(y_pitch, "Length")<<" "<<voltage<<" "<<x_pixel_offset<<" "<<y_pixel_offset<<" "<<temperature<<"\n";
-//		std::cout << "      sigma " << G4BestUnit(sigma, "Length")<<"\n";
-//	}
-
-	if (sigma == 0)
+	if (sigma == 0)  // tread not defined calculation input
 		return 1.;
 	return CalcBivarianteNormalCDFWithLimits(x_pitch * (x_pixel_offset - 1. / 2.), x_pitch * (x_pixel_offset + 1. / 2.), y_pitch * (y_pixel_offset - 1. / 2.), y_pitch * (y_pixel_offset + 1. / 2.), x, y, sigma);
-}
-
-double Digitizer::CalcBivarianteNormalCDFWithLimits(const double& a1, const double& a2, const double& b1, const double& b2, const double& mu1, const double& mu2, const double& sigma)
-{
-// Calculates the integral of the bivariante normal distribution between x = [a1, a2], y = [b1, b2]. The normal distribution has two mu: mu1, mu2 but only one common sigma.
-	return 1. / 4. * (erf((a2 - mu1) / std::sqrt(2 * sigma * sigma)) - erf((a1 - mu1) / std::sqrt(2 * sigma * sigma))) * (erf((b2 - mu2) / std::sqrt(2 * sigma * sigma)) - erf((b1 - mu2) / std::sqrt(2 * sigma * sigma)));
 }
 
 double Digitizer::CalcSigmaDiffusion(const double& length, const double& voltage, const double& temperature)
@@ -223,19 +214,57 @@ double Digitizer::CalcSigmaDiffusion(const double& length, const double& voltage
 	 temperature : the temperature in Kelvin
 	 */
 	const double kb_K_e = 8.6173e-5;  // Boltzmann Constant * Kelvin / elemetary charge
-	return length * std::sqrt(2 * temperature / voltage * kb_K_e);
+	return length * std::sqrt(2. * temperature / voltage * kb_K_e);
+}
+
+double Digitizer::CalcBivarianteNormalCDFWithLimits(const double& a1, const double& a2, const double& b1, const double& b2, const double& mu1, const double& mu2, const double& sigma)
+{
+// Calculates the integral of the bivariante normal distribution between x = [a1, a2], y = [b1, b2]. The normal distribution has two mu: mu1, mu2 but only one common sigma.
+	return 1. / 4. * (erf((a2 - mu1) / std::sqrt(2 * sigma * sigma)) - erf((a1 - mu1) / std::sqrt(2 * sigma * sigma))) * (erf((b2 - mu2) / std::sqrt(2 * sigma * sigma)) - erf((b1 - mu2) / std::sqrt(2 * sigma * sigma)));
+}
+
+double Digitizer::CalcZfromSigma(const double& z)
+{
+	// To simulate the initial charge cloud the z-position of the hit is scales up to
+	// give a gaussian with sigma0 at the old z-position.
+	// The new z cannot be calculated analytically
+	// sigma^2 * ln(z / z_new) - sigma0^2 = 0 has to be solved numerically
+	for(double zNew = z; zNew < z + fSensorThickness; zNew += 0.5 * um){  // increase z_new until close solution is found
+		double sigma = CalcSigmaDiffusion(zNew, fBias, fTemperatur);
+		double minimizeme = 2 * sigma * sigma * std::log(zNew / z) - fSigma0 * fSigma0;  // function to minimize
+		if (minimizeme > 0)  // function has 0 crossing, take this as the minimum
+			return zNew;
+	}
+
+	// No z new was found, should not occur!
+	std::cout<<"\nEVENT: "<<G4EventManager::GetEventManager()->GetConstCurrentEvent()->GetEventID()<<std::endl;
+	G4cout << "    z " << G4BestUnit(z, "Length") << G4endl;
+	G4cout << "    fSigma0 " << G4BestUnit(fSigma0, "Length") << G4endl;
+
+	G4Exception("Digitizer::CalcZfromSigma()", "MyCode0004", FatalException, "Cannot calculate hit z-position for initial charge cloud simulation. Decrease sigma0.");
+	return 0;
 }
 
 void Digitizer::PrintSettings()
 {
 	G4cout << "Digitization settings: " << G4endl;
-	G4cout << "  Calculate charge cloud\t" << fCalcChargeCloud << G4endl;
+	if (fCalcChargeCloud){
+		G4cout << "  Calculate charge cloud: yes\t" << G4endl;
+		G4cout << "    Bias\t" << fBias << G4endl;
+		G4cout << "    Temperature\t" << fTemperatur << G4endl;
+		G4cout << "    Init. Sigma\t" << G4BestUnit(fSigma0, "Length") << G4endl;
+		G4cout << "    Sigma Corr. Factor\t" << fSigmaCC << G4endl;
+	}
+	else
+		G4cout << "  Calculate charge cloud: no\t" << G4endl;
 	G4cout << "  Digitization parameters: " << G4endl;
 	G4cout << "    Thickness\t" << G4BestUnit(fSensorThickness, "Length") << G4endl;
 	G4cout << "    Number of columns\t" << fNcolumns << G4endl;
 	G4cout << "    Column width\t" << G4BestUnit(fPixelPitchX, "Length") << G4endl;
 	G4cout << "    Number of rows\t" << fNrows << G4endl;
 	G4cout << "    Row width\t" << G4BestUnit(fPixelPitchY, "Length") << G4endl;
+	G4cout << "    Threshold\t" << fThreshold << G4endl;
+	G4cout << "    Noise\t" << fNoise << G4endl;
 }
 
 void Digitizer::SetEnergyPerCharge(const G4double& energyPerCharge)
@@ -273,5 +302,17 @@ void Digitizer::SetSensorZdirection(const bool& direction)
 		std::cout << "Set read out electronics behind sensor" << std::endl; // FIXME: G4cout + MT not working
 	else
 		std::cout << "Set read out electronics before sensor" << std::endl; // FIXME: G4cout + MT not working
+}
+void Digitizer::SetInitChargeCloudSigma(const G4double& sigma0)  // the initial charge cloud is not zero due to repulsion
+{
+	fSigma0 = sigma0;
+	std::cout << "Set the initial charge cloud sigma to " << G4BestUnit(fSigma0, "Length") << std::endl; // FIXME: G4cout + MT not working
+}
+void Digitizer::SetChargeCloudSigmaCorrection(const G4double& sigmaCC)  // the sigma is higher due to repulsion, so correct sigma with factor > 0; very simple approximation; for further info see NIMA 606 (2009) 508-516
+{
+	if (sigmaCC > 0.5){
+		fSigmaCC = sigmaCC;
+		std::cout << "Set charge cloud sigma correction factor to " << sigmaCC << std::endl; // FIXME: G4cout + MT not working
+	}
 }
 
